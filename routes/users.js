f6899df0e476ad80b2641ee90b2e45b1c73b7cad
yukinameko <g1813551@gmail.com>
2019-03-05 23:19:32 +0900

    [update] /users/(id)のURLへのアクセスでユーザデータが表示されるページの追加

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const con = require('../mysql-script/connect.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/:id([0-9]+)', (req, res, next) => {
+	const params = req.params;
+	console.log(params);
+
+	con.connect(err => {
+		if(err) console.log(err);
+		console.log('connected');
+
+		con.query('select * from data.users where id = ?', [params.id], (err, result) => {
+			if(result == undefined) res.render('users', {id:0, username:null, twitterid:null});
+			// con.end();
+			res.render('users', result[0]);
+		});
+	});
+});
+
+router.post('/:id([0-9]+)', (req, res) => {
+	const params = req.params;
+	con.connect(err => {
+		if(err) console.log(err);
+		console.log('connected');
+
+		con.query('select * from data.users where id = ?', [params.id], (err, result) => {
+			// con.end();
+			res.send(result);
+		});
+	});
+})
 
 module.exports = router;
